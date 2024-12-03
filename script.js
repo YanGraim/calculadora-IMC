699cc0f1010391913817396341969b01a7603ff2
YanGraim <yangraim@gmail.com>
2024-12-03 15:12:09 -0300

    implementa função e logica no javascript

--- script.js
+++ script.js
@@ -9,11 +9,22 @@
 
 function calcular(event) {
     event.preventDefault();
-    
+    let resultado;
     let peso = document.getElementById('peso').value;
     let altura = document.getElementById('altura').value;
 
-
     let imc = peso/(altura*altura);
-    console.log(imc);
+    if(imc < 17) {
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<p> Seu resultado foi: <strong>${imc}</strong></p><p>Você está <strong>muito abaixo</strong> do peso</p>`
+    }else if(imc > 17 && imc <= 18.49) {
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<p> Seu resultado foi: <strong>${imc}</strong></p><p>Você está <strong>abaixo</strong> do peso</p>`
+    }else if(imc > 18.5 && imc <= 24.99) {
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<p> Seu resultado foi: <strong>${imc}</strong></p><p>Você está com o peso <strong>normal</strong></p>`
+    } else {
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = `<p> Seu resultado foi: <strong>${imc}</strong></p><p>Você está <strong>acima</strong> do peso</p>`
+    }
 }
